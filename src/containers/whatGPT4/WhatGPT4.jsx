849a8c80859b3cff8643eae4ab563b07f38f77b5
FarhanK340 <farhankashif6@gmail.com>
2024-04-16 22:04:06 +0500

    Navbar andHeader Components

---
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './whatGPT4.css';
+import { Feature } from '../../components';
+
+const WhatGPT4 = () => {
+  return (
+    <div className='gpt4__whatgpt4 section__margin' id='wgpt4'>
+      <div className="gpt4__whatgpt4-features">
+        <Feature />
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT4
